ab806a9c749b381a575cb251585a4647fa80cac8
Salim Mansuri <salimmansuri27@gmail.com>
2025-09-27 15:59:55 -0700

    Initial
    Initial Programming Assignment - Salim

---
+++ Module 1/WorkflowDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Author: 
+Date:
+Assignment: 
+*/
+
+
+namespace WorkflowDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("(^._.^)");
+            Console.WriteLine("( o.o )");
+        }
+    }
+}
